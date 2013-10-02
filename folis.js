ac2752c51271d652f3c8c28a6338eb34d84df424
Kyle Tolle <kyle@nullsix.com>
2013-10-02 15:45:35 -0600

    Rename constants

--- folis.js
+++ folis.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
 var app = require('http').createServer(handler);
-var listeningPort = process.env.PORT || 9000;
-var statusCode = 200;
+var LISTENING_PORT = process.env.PORT || 9000;
+var STATUS_CODE = 200;
 var sleep = require('sleep');
-var secondsToSleep = 0;
+var SECONDS_TO_SLEEP = 0;
 var requests = {}
 var count = 0;
 var requestsToFileTimer;
-var DISK_WRITE_DELAY = 10000;
+var DISK_WRITE_DELAY = 5000;
 
-app.listen(listeningPort);
+app.listen(LISTENING_PORT);
 
 function handler (req, res) {
   var data = '';
@@ -28,13 +28,13 @@ function handler (req, res) {
     });
   }
 
-  res.writeHead(statusCode, {'Content-Type': 'text/plain'});
-  sleep.sleep(secondsToSleep);
+  res.writeHead(STATUS_CODE, {'Content-Type': 'text/plain'});
+  sleep.sleep(SECONDS_TO_SLEEP);
   res.end();
 }
 
-console.log("Listening to port " + listeningPort);
-console.log("Returning status code " + statusCode);
+console.log("Listening to port " + LISTENING_PORT);
+console.log("Returning status code " + STATUS_CODE);
 console.log();
 
 function sendRequestsToFile() {
